eab8e5416d3a73d8ea6a01e4a467685db05c4206
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2018-10-30 19:36:15 +0100

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer1 = "test 1";
+            Console.WriteLine($"Answer 1: {answer1}");
+
+            var answer2 = "test 2";
+            Console.WriteLine($"Answer 2: {answer2}");
+
+            Console.ReadKey();
+        }
+    }
+}
